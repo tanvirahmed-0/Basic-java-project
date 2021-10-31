b68755383a46690c8d511bb571c7dbed0ef2a123
Tanvir Ahmed <93043879+tanvirahmed-0@users.noreply.github.com>
2021-10-31 14:39:23 +0600

    Add files via upload

---
+++ Java project/script.js
@@ -0,0 +1,47 @@
+alert("Welocome");  //Popup massege.
+ console.log("Ami akta balok"); //Console output.
+//variable declear.
+ var a,b,c;
+ a = 23;
+ b = 36
+ c = a + b; 
+ document.write(c);
+//call id from html.
+ document.getElementById("test").innerHTML = 8 + 7;
+
+//variable declear.
+var  name, number, address;
+
+       nam = "Tanvir Ahmed <br>";
+       console.log(typeof(nam));  //value type check.
+
+       number = "01717833212 <br>";
+       number = parseInt(number); //convert value type.
+       console.log(typeof(number));
+
+       address = "Gobindaganj, Gaibandha <br>";
+ document.write(nam, number, address);
+
+ var x = 2.52443;
+
+ console.log(x.toFixed(2));//toFixed method.
+ console.log(x.toPrecision(4));//toPrecision method.
+ console.log(typeof(Number(        "35"       )));//direct convert int. to number and check value type. 
+ console.log(Number("35"));//direct convert int. to number.
+
+
+ document.write("Tanvir" + " Ahmed" + " Ranveer <br>");
+
+ var firstName = "Tanvir";
+ var lastName = " Ahmed";
+
+ var fullName = firstName + lastName;
+ document.write("My name is " + fullName);
+
+
+ var x = "Ranveer JD";
+ document.write(x + " is my name <br>");
+
+ var num1 = 20;
+ var num2 = 33;
+document.write("num1 = " + num1 + " and " + "num2 = " + num2);
